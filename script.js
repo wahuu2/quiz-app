b418b516d07fb0c3e661d7615981ae7175fa0cf7
wahuu2 <lilywahu1@gmail.com>
2026-05-29 02:02:47 +0300

    script.js

--- script.js
+++ script.js
@@ -1267,3 +1267,14 @@ maxScoreSpan.textContent = totalQuestions; // show overall total
 // event listeners
 startButton.addEventListener("click", startQuiz);
 restartButton.addEventListener("click", restartQuiz);
+function startQuiz() {
+  // reset vars
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreSpan.textContent = 0;
+
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
+}
